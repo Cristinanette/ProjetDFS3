f947410515fbc27e870fe327c3b4743d76f684d6
cristina.grandjean <cristina.grandjean@centrale-marseille.fr>
2019-11-27 23:28:40 +0100

    adding variables and animation

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
         require('postcss-import'),
         require('tailwindcss'),
+        require('postcss-variables')({
+            globals: {
+                background: '#f7fafc'
+            }
+        }),
+        require('autoprefixer'),
+        purgecss({
+            content: ['./*.html']
+        }),
     ]
 };
